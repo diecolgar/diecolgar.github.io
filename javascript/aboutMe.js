0b9164c5e09c9afc9c9518215813ef15ee70fb26
diecolgar <diecolgar@gmail.com>
2022-01-15 21:07:30 +0100

    15 January 2022

--- javascript/aboutMe.js
+++ javascript/aboutMe.js
@@ -1,6 +1,9 @@
 const myStoryButton = document.querySelector('.mystorybutton')
 const myStory = document.querySelector('.mystory')
+const Story = document.querySelectorAll('.story')
 const aboutMeBlur = document.querySelector('.blur')
+const iteratorBall = document.querySelectorAll('.iterator-ball')
+const iteratorBallFilled = document.querySelector('.iterator-ball-filled')
 
 myStoryButton.addEventListener('click', () => {
     myStoryButton.classList.toggle('opened')
@@ -9,4 +12,17 @@ myStoryButton.addEventListener('click', () => {
         horizontalPosition = 30
     }
     moveSlide()
+})
+
+iteratorBall.forEach((iteratorBall, id) => {
+    iteratorBall.addEventListener('click', () => {
+        iteratorBallFilled.style.left = `${(id*2.1)}vw`
+        Story.forEach((Story, storyId) => {
+            Story.style.opacity = 0
+            if (id == storyId) {
+                Story.style.opacity = 1
+            }
+        })
+    })
+
 })
