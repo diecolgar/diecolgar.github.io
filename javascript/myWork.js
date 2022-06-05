e069334d051e5802b114d97b128aec932ffb1f75
diecolgar <diecolgar@gmail.com>
2022-06-05 23:59:01 +0200

    New design concept on my work tab: proximity sublogic

--- javascript/myWork.js
+++ javascript/myWork.js
@@ -1,57 +1,26 @@
-const displayableItems = document.querySelectorAll('.mywork .displayer div')
-
-const iosDisplayable = document.querySelector('.mywork .displayer .ios')
-const minipeopleDisplayable = document.querySelector('.mywork .displayer .minipeople')
-
-const myWorkItems = document.querySelectorAll('.mywork .container .item')
-const myWorkItemsContent = document.querySelector('.mywork .content')
-
-let itemsLength = myWorkItems.length
-
-let motion
-let newPosition
-
-let positionArray = ['first', 'second', 'third', 'fourth', 'fifth']
-
-function displayCurrentItem() {
-
-    displayableItems.forEach( dispItem => {
-        dispItem.classList.remove('active')
-    })
-
-    myWorkItems.forEach((item,id) => {
-        if (item.classList.contains('third')) {
-            if(item.classList.contains('ios')) {
-                iosDisplayable.classList.add('active')
-            }
-            if(item.classList.contains('minipeople')) {
-                minipeopleDisplayable.classList.add('active')
-            }
-        }
-    })
-}
+const myworkselectables = document.querySelectorAll(".myworkselectables .selectable")
 
 
-myWorkItems.forEach((item,id) => {
-    item.addEventListener('click', () => {
-        motion = 2 - id
-        console.log(motion)
-
-        myWorkItems.forEach((item2,id2) => {
-            item2.classList.remove('first')
-            item2.classList.remove('second')
-            item2.classList.remove('third')
-            item2.classList.remove('fourth')
-            item2.classList.remove('fifth')
-            newPosition = id2 + motion
-            if ( (4 >= newPosition) && (newPosition >= 0) ) {
-                item2.classList.add(positionArray[newPosition])
-            } else if ( newPosition < 0) {
-                item2.classList.add(positionArray[5 + newPosition])
-            } else if ( newPosition > 4) {
-                item2.classList.add(positionArray[newPosition - 5])   
-            }
-        })
-        displayCurrentItem()
-    })
+window.addEventListener('mousemove', function(event)
+{
+    var x = event.clientX;
+    var y = event.clientY;
+    windowX = window.innerWidth;
+    windowY = window.innerHeight;
+    console.log(`hor ${x} and width ${windowX}`);
+    console.log(`ver ${y} and width ${windowY}`);
+    myworkselectables.forEach((selectable, id) => {
+        var selRect = selectable.getBoundingClientRect();
+        var selectX = (selRect.left + selRect.right)/2;
+        var selectY = (selRect.top + selRect.bottom)/2;
+        var distanceX = Math.abs(x - selectX);
+        var distanceY = Math.abs(y - selectY);
+        selectable.style.opacity = 0.5 - distanceX*0.0004 - distanceY*0.0004;
+    });
 })
+
+
+
+
+
+
